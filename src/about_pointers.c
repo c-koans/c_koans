36bb168ef7877ac65e64121c3445c6cf440e7f84
Brian Gavin <bgavin@cs.stonybrook.edu>
2017-08-08 14:11:46 -0400

    pointer koans

--- src/about_pointers.c
+++ src/about_pointers.c
@@ -134,25 +134,3 @@ Test(c_koans, function_pointers) {
     cr_assert(strcmp(names[0], ordered_names[0]) == 0,
 	      "Names are not sorted. Try again.");
 }
-
-static int string_compare(const void *s1, const void *s2) {
-    /*
-     * The comparison function must match the declaration in the prototype
-     * exactly. This is why, even though we are comparing 2 strings, the
-     * arguments must be void pointers.
-     *
-     * The actual arguments passed into this function is the address of each
-     * element in the array, which in this case is a char **, this is why we
-     * must cast the argument to a char ** and dereference it once for strcmp
-     * to work.
-     */
-    return strcmp(*(char **)s1, *(char **)s2);
-}
-
-static void double_an_int(int *i) {
-	/* The '*' operator for dereference has a higher precedence than the other
-	 * arithmetic operators, therefore it will be muliplying and assigning the
-	 * int that is pointer to by i.
-	 */
-	*i *= 2;
-}
