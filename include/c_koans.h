06334f7a6fcf540c89d46c14090195851be2d1d7
Brian Gavin <bgavin@cs.stonybrook.edu>
2017-08-10 17:18:39 -0400

    about functions, feel as though i should add more

--- include/c_koans.h
+++ include/c_koans.h
@@ -4,8 +4,14 @@
 #define TODO 0
 #define TODO_NZ 1
 
+extern int global_var;
+
 int string_compare(const void *p1, const void *p2);
 
 void double_an_int(int *i);
+
+int modify_global();
+int modify_local();
+int modify_local_static();
 
 #endif
--- src/c_koans_helpers.c
+++ src/c_koans_helpers.c
@@ -1,6 +1,8 @@
 #include "c_koans.h"
 #include <string.h>
 
+int global_var = 0;
+
 int string_compare(const void *s1, const void *s2) {
     /*
      * The comparison function must match the declaration in the prototype
@@ -21,4 +23,35 @@ void double_an_int(int *i) {
 	 * int that is pointer to by i.
 	 */
 	*i *= 2;
+}
+
+int modify_global() {
+    /*
+     * We modify the global variable, located in the .text section,
+     * visible to the entire program.
+     */
+    global_var++;
+    return global_var;
+}
+
+int modify_local() {
+    /*
+     * We modify the local variable, located and initialzed on the stack.
+     * every call, it will be initalized and modified in the same fashion.
+     */
+    int i = 0;
+    i++;
+    return i;
+}
+
+int modify_local_static() {
+    /*
+     * Local static variables will be initialized only once and be located
+     * in the .text section. Local static variables can only be referenced
+     * inside the function because the name will be known inside the function.
+     * This causes the value of the variable to be preserved across function calls.
+     */
+    static int i = 0;
+    i++;
+    return i;
 }
