658ea994fc6744797bf0791bc24535d264793cf5
may <mehrabhoque@gmail.com>
2017-08-12 22:56:05 -0400

    Add more koan code for learning about arrays.
    Added some examples of array use, including:
      1. Braced initializer list syntax.
      2. Dynamically resizable arrays.
      3. Strings as arrays.

--- src/about_arrays.c
+++ src/about_arrays.c
@@ -1,3 +1,8 @@
+#include "c_koans.h"
+
+// TODO: remove
+#include <stdio.h>
+
 #include <criterion/criterion.h>
 
 void func(int* array){
@@ -50,4 +55,63 @@ Test(c_koans, what_is_an_array){
                                    "of a pointer or sum of all memory the "
                                    "array takes up?");
     func(array); // Goto line 3
+
+    /* You do not always have to take up so much lines to create an array whose
+     * contents you already know. You can use {...} syntax to create such an
+     * array elegantly. */
+    int another_array[5] = {1, 2, 3, 4, 5};
+    cr_assert_eq(another_array[3], TODO, "We should be seeing the some "
+                                         "element's value.");
+
+    /* You will not always know how many items an array might hold at runtime.
+     * In these kinds of scenarios, you can't use the syntax we've been using.
+     * Instead, you'll have to think about the array as a pointer first, and
+     * then you can start using it as an array again. */
+    const size_t INIT_ARR_SIZE = 5;
+    int* yet_another_array = calloc(INIT_ARR_SIZE, sizeof(int));
+    /* Thankfully, the syntax is just like it was before. */
+    unsigned i;
+    for(i = 0; i < INIT_ARR_SIZE; i++){
+      /* You can loop on arrays, as long as you handle the indexing logic
+       * correctly. */
+      yet_another_array[i] = i + 1;
+    }
+
+    /* When we need to, we can make an array bigger to accomodate via realloc.
+     */
+    if(!realloc(yet_another_array, INIT_ARR_SIZE * sizeof(int))){
+      exit(1);
+    }
+
+    yet_another_array[INIT_ARR_SIZE] = 6;
+    unsigned where = 0;
+    for(i = 0; i < INIT_ARR_SIZE+1; i++){
+      if(yet_another_array[i] == INIT_ARR_SIZE+1){
+        where = i;
+      }
+
+      cr_assert_eq(yet_another_array[i], TODO, "Although we started with an "
+                                               "array of 5 elements, we "
+                                               "should be able to find a "
+                                               "sixth element as well.");
+    }
+    cr_assert_eq(where, TODO, "We should be seeing a certain value, given the "
+                              "way we set these elements' values.");
+
+    /* Often, we find ourselves in need of representing strings in a program,
+     * and usually, we are able to use something like a String class or type.
+     * Unfortunately, C does not have any such notion of a string baked into
+     * the language like in some other languages (e.g. Java). */
+    const char a_string[13] = "hello world!"; // This is a 'string' in C.
+
+    /* In C, a string is simply an array of characters. */
+    cr_assert_eq(a_string[3], TODO, "We may be interested in a particular "
+                                    "character of strings.");
+
+    /* In C, memory is just memory. It is just a bunch of bytes. Therefore, we
+     * need to be able to know when a string ends. We use something called a
+     * 'null byte' or 'null terminator', which is really just a zero-byte, to
+     * do this. This byte is always found at the end of a string, and if it is
+     * missing, can lead to very dangerous and unpredictable bugs. */
+    cr_assert_eq(a_string[12], TODO, "Null terminators are essential!");
 }
