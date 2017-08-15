d1c3d9be1e45c25dbb6eac1b8eed48fff6109998
Brian Gavin <bgavin@cs.stonybrook.edu>
2017-08-15 15:07:23 -0400

    about_io

--- src/about_io.c
+++ src/about_io.c
@@ -1,9 +1,6 @@
 #include <criterion/criterion.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <criterion/redirect.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
 #include "c_koans.h"
 
 Test(about_io, intro) {
@@ -26,11 +23,19 @@ Test(about_io, intro) {
         http://www.cplusplus.com/reference/cstdio/printf/
         Complete the following:
     */
+
+    /*
+        Use the references to determine which format specifier will give the
+        proper output. When you complete this koan, change the literal on line
+        37 to a nonzero value. This is to avoid compilation errors.
+    */
+
+#if 0
     char c = 'J';
     int i = -1;
     double d = 3.14;
     long l = 0xDEADBEEF;
-    void* p = 0x400;
+    void* p = (void*)0x400;
 
     printf("char: TODO\n", c);
     printf("int: TODO\n", i);
@@ -39,8 +44,34 @@ Test(about_io, intro) {
     printf("float: TODO\n", d);
     printf("long: TODO\n", l);
     printf("pointer: TODO\n", p);
-
+#endif
     cr_assert_file_contents_eq_str(stdout, "char: J\nint: -1\nunsigned int:    \
     4294967295\nhexadecimal unsigned int: ffffffff\nfloat: 3.140000\nlong:     \
     3735928559\npointer: 0x400\n");
 }
+
+Test(about_io, file_stream) {
+    /*
+        The standard io library, <stdio.h>, provides a FILE type for standard io
+        usages. Most functions that use standard in or out have a stream counter
+        -part, making use of FILE*.
+    */
+
+    FILE *f;
+    char buf[1024];
+    /*
+        fopen takes a path to a file and a mode, in this case we are opening in
+        read only mode.
+    */
+    f = fopen("rsrc/file.txt", "r");
+
+    /*
+        fgets will read at most the number of bytes specified into the given
+        buffer from the stream. fgets will read until it reads the number of
+        bytes, it hits a new line character or until it gets EOF.
+    */
+    fgets(buf, 1024, f);
+
+    cr_assert_str_eq(buf, TODO_S, "rsrc/file.txt to see what buf should be");
+}
+
