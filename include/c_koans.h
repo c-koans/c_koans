c9b11aa2f7981daac4d04b01b6410d10c7aaee0e
Brian Gavin <bgavin1996@gmail.com>
2018-01-29 16:28:27 -0500

    separate about_io into about_printing and a new about_io

--- include/c_koans.h
+++ include/c_koans.h
@@ -4,6 +4,7 @@
 #define TODO 0
 #define TODO_NZ 1
 #define TODO_S ""
+#define TODO_FP (FILE*)!NULL
 
 typedef struct {
     int month;
--- src/about_io.c
+++ src/about_io.c
@@ -3,61 +3,103 @@
 #include <criterion/redirect.h>
 #include <stdio.h>
 
-Test(about_io, intro)
+/*
+    This series of koans will go over topics not entirely specific to the C
+    language itself, but is an essential topic for writing more-than-trivial
+    C programs: IO. It is recommended to do the about_printing koan prior to
+    this one.
+
+    As IO is fairly environment specific, this koan will only go over the
+    standard IO library that is supported by all platforms.
+
+    It will be referred to as stdio, after the header file <stdio.h>
+ */
+
+Test(about_io, streams)
 {
     /*
-        This koan will teach you about input and output in C. Let's go over the
-        basics of printing out text to the terminal with a format. This is
-        one of the most common ways of printing information out in C.
-    */
-    /* NOTE: Don't forget the newline character (\n)! */
-    printf("Hello World!\n");
+        Before we can go into detail about stdio we must discuss the
+	foundation of the library, the three streams that are provided:
+	stdin, stdout, and stderr
 
-    /*
-        To print variables you must use a % modifier in the format string passed
-        to printf. For example:
-    */
-    char *string = "World!";
-    printf("Hello %s\n", string);
-
-    /*
-        There are many modifiers present in C. You can access a list of them at:
-        http://www.cplusplus.com/reference/cstdio/printf/
-        Complete the following:
+	Don't worry if you cannot fully understand these concepts, we will go
+	into more detail about them as we go along.
     */
 
     /*
-        Use the references to determine which format specifier will give the
-        proper output. When you complete this koan, change the literal on line
-        37 to a nonzero value. This is to avoid compilation errors.
+        stdin provides input, typically keyboard input from your terminal.
+
+	The functions that interact with stdin will typically block the program
+	waiting	until input is entered into the program.
     */
 
-#if 0
-    char c = 'J';
-    int i = -1;
-    double d = 3.14;
-    long l = 0xDEADBEEF;
-    void* p = (void*)0x400;
+    cr_assert_str_eq("stdio", TODO_S, "The standard input stream is...");
 
-    printf("char: TODO\n", c);
-    printf("int: TODO\n", i);
-    printf("unsinged int: TODO\n", i);
-    printf("hexadecimal unsigned int: TODO\n", i);
-    printf("float: TODO\n", d);
-    printf("long: TODO\n", l);
-    printf("pointer: TODO\n", p);
-#endif
-    cr_assert_file_contents_eq_str(stdout,
-        "char: J\nint: -1\nunsigned int: 4294967295\nhexadecimal unsigned int: "
-        "ffffffff\nfloat: 3.140000\nlong: 3735928559\npointer: 0x400\n");
+    /*
+        stdout provides output, which is typically your terminal screen.
+    */
+
+    cr_assert_str_eq("stdout", TODO_S, "The standard output stream is...");
+
+    /*
+        stderr is slightly different than stdin and stdout. This stream provides
+	a stream to write error messages to. Under normal execution, it usually
+	prints to the same place as stdout.
+
+	Since it is a different stream entirely, tricks can be done such that
+	it will instead write to a different place entirely, or its ouput is
+	ignored and will not interfere with the regular output of a program.
+
+	The aforementioned "tricks" are not related to the C language, so they
+	will not be discussed in this koan, but it is an important topic for
+	more advanced C programming.
+    */
+
+    cr_assert_str_eq("stderr", TODO_S,
+		     "The output stream for error messages is...");
 }
 
-Test(about_io, file_stream)
+Test(about_io, using_streams)
 {
     /*
-        The standard io library, <stdio.h>, provides a FILE type for standard io
-        usages. Most functions that use standard in or out have a stream counter
-        -part, making use of FILE*.
+        Now that we are familiar with the three streams given to us, we can go
+	over the functions that utilize streams.
+
+	Most of the functions that we have seen before that use stdout by
+	default have stream-using counterparts.
+
+	For simplicity, we will use fputs, the stream counterpart of puts.
+    */
+
+    /*
+        For this koan, output "Hello World" to both stdout and stderr.
+	<stdio.h> provides variables for accessing the streams, which are just
+	the names that we have been referring to them until now
+	(stdin, stdout, and stderr)
+    */
+
+    fputs("Hello World", TODO_FP);
+    fputs("Hello World", TODO_FP);
+
+    cr_assert_file_contents_eq_str(stdout, "Hello World");
+    cr_assert_file_contents_eq_str(stderr, "Hello World");
+
+    /*
+        "But what exactly is a stream?" you may ask. This question will be
+	answered in the next koan.
+    */
+}
+
+Test(about_io, file_io)
+{
+    /*
+        The standard io library, <stdio.h> provides a FILE type for standard io
+        usages. Any pointer of type FILE* is referred to as a "stream." So, this
+	means that the variables we have been using thus far have been type
+	FILE*.
+
+	Streams can be used for our own file input and output. This koan will
+	go over opening from and reading from a file.
     */
 
     FILE *f;
@@ -75,5 +117,33 @@ Test(about_io, file_stream)
     */
     fgets(buf, 1024, f);
 
-    cr_assert_str_eq(buf, TODO_S, "rsrc/file.txt to see what buf should be");
+    cr_assert_str_eq(buf, TODO_S, "view rsrc/file.txt to see what buf should be");
+}
+
+Test(about_io, buffered_io)
+{
+    /*
+        One of the most important aspects of the standard io library is that
+	it is buffered. What does this mean? It means that every write is not
+	immediatly written to the stream, for efficiency purposes.
+
+	Every write is first written to a buffer in memory before it is written
+	to the stream, and is instead all written together in one big write at
+	a later time. Since IO is expensive, reducing the number of times these
+	operations are done saves quite a bit of time.
+
+	Reads on the other hand are done in large chunks, so reading from a
+	stream will be reading from a buffer in memory.
+
+	To send what's in the buffer to the stream without waiting, stdio has
+	a fuction fflush that will "flush" a stream's buffer.
+    */
+
+    /*
+        For this koan, output "foo" to stdout. This may seem trivial, but the
+	added "fflush" changes this operation quite a bit under the hood.
+    */
+    fputs(TODO_S, TODO_FP);
+    fflush(stdout);
+    cr_assert_file_contents_eq_str(stdout, "foo");
 }
